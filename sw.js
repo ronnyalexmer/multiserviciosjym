0f4e15fd1f3eacba39b08613f81be62228cabcd9
chuwi tablet <ronny.medina.eras@outlook.com>
2023-05-03 12:18:28 +0200

    Add some code

--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
 const cacheName = "site-static";
 const assets = [ 
-    "/", "/index.html", "/src/js/app.js", "/src/css/styles.css"
+    "/", "/index.html", "/src/js/app.js", "/src/css/styles.css",
+    "/src/elements/head.html", "/src/elements/header.html",
+    "/src/elements/footer.html", "/src/img/logo.png",
+    "/pages/bricolaje.html", "/pages/electricidad.html",
+    "/pages/fontaneria.html"
 ]
 
 self.addEventListener("install", event => {
-    //console.log("SW has been installed");
     event.waitUntil(
         caches.open(cacheName).then(cache => {
-            console.log("Caching assets");
             cache.addAll(assets);
         })
     );
