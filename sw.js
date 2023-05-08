9ef40cd6b957e7d8e4bed28359def4b697d983b2
chuwi tablet <ronny.medina.eras@outlook.com>
2023-05-08 22:59:24 +0200

    home done

--- sw.js
+++ sw.js
@@ -2,9 +2,9 @@ const cacheName = "site-static";
 const assets = [
     "/", "/index.html", "/src/js/app.js",
     "/src/css/lib/bootstrap.min.css", "/src/css/styles.css", "/src/css/contact-form.css", "/src/css/footer.css",
-    "/src/css/included-services.css", "/src/css/menu-navbar.css", "/src/css/services-section.css", "/src/css/services-social-navbar.css",
-    "/src/img/logo.jpg", "/src/img/fotos/contact-form.jpg", "/src/img/section-bricolaje.jpg", "/src/img/section-electricidad.jpg",
-    "/src/img/section-fontaneria.jpg", "/src/img/section-climatizacion.jpg", "/src/img/section-persianas.jpg", "/src/img/section-montajes.jpg",
+    "/src/css/included-services.css", "/src/css/menu-navbar.css", "/src/css/services-section.css", "/src/css/services-social-navbar.css", "/src/css/about-us.css",
+    "/src/img/logo.jpg", "/src/img/fotos/contact-form.jpg", "/src/img/section-instalaciones.jpg", "/src/img/section-electricidad.jpg",
+    "/src/img/section-fontaneria.jpg", "/src/img/section-climatizacion.jpg", "/src/img/section-reformas.jpg", "/src/img/section-montajes.jpg",
     "/src/img/svg/ico-booked.svg", "/src/img/svg/ico-budget.svg", "/src/img/svg/ico-email.svg", "/src/img/svg/ico-instagram-color.svg", "/src/img/svg/ico-instagram.svg",
     "/src/img/svg/ico-taskrabbit-color.svg", "/src/img/svg/ico-taskrabbit.svg", "/src/img/svg/ico-tlf.svg", "/src/img/svg/ico-transport.svg", "/src/img/svg/ico-whatsapp-color.svg", 
     "/src/img/svg/ico-whatsapp.svg", "/src/img/svg/logo-ico.svg"
